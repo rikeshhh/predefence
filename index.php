d40124fc63195e4bfdd88771eab08842d58984b9
rikesh <70895896+rikeshhh@users.noreply.github.com>
2023-02-23 10:29:33 +0545

    frontend for predefence

---
+++ index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/945fb9bd69.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="main.css" />
+    <title>Document</title>
+  </head>
+  <body>
+   <?php
+   include './navigatiuon/header.html';
+   
+   ?>
+    <div class="containerOne">
+      <div class="content">
+        <div class="rowOne">
+          <h1>Perfect plant on sale for your Home</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, aut
+            recusandae! Maxime non minus ducimus. Numquam, in! Consectetur eius
+            inventore, aspernatur accusamus enim facere at officia dolores error
+            a modi.
+          </p>
+          <button><a href="./allcarts/allcarts.html">Shop Collection</a></button>
+        </div>
+        <div class="image">
+        <div class="pic-ctn">
+    <img src="" alt="" class="pic">
+    <img src="./image/andrew-reshetov-tXGOhiGBu5I-unsplash.jpg" alt="" class="pic">
+    <img src="./image/j2.jpg" alt="" class="pic">
+    <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" alt="" class="pic">
+    <img src="./image/boxed-water-is-better-M6eWvLb2EYY-unsplash.jpg" alt="" class="pic">
+  </div>
+
+         
+        </div>
+      </div>
+    </div>
+   
+    <div class="journey">
+      <div class="journeyContent">
+        <img src="./image/j1.jpg" alt="" class="jcOne" />
+
+        <img src="./image/j2.jpg" alt="" class="jcTwo" />
+
+        <div class="jcThree">
+          <h1>Our Journey</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            rem soluta doloremque modi aperiam. Ab, placeat. Distinctio deleniti
+            temporibus quis, deserunt necessitatibus in fugiat, eos nulla harum
+            recusandae eius magnam.
+          </p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
+    <div class="why">
+        <div class="whyOne">
+          <h1>Why shop with us?</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et dolore
+            aliquid animi at tenetur, sequi amet nulla illum sit, eos minus?
+            Delectus error ipsa alias exercitationem, rerum dolorum facilis
+            libero.
+          </p>
+          <ol>
+            <li>Soculents</li>
+            <li>Air Purifier</li>
+            <li>Decorative</li>
+          </ol>
+        </div>
+        <div class="whyTwo">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+        </div>
+      </div>
+  
+    <div class="arrival" >
+      <h1>Flower</h1>
+      <div class="freshArrival" id="flower">
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Chinese Money Plant</p>
+          <p>Rs 500</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/andrew-reshetov-tXGOhiGBu5I-unsplash.jpg" />
+
+          <p>Cactud</p>
+          <p>Rs 600</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/andrew-reshetov-tXGOhiGBu5I-unsplash.jpg" />
+
+          <p>Calathea</p>
+          <p>Rs 5500</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Aloevera</p>
+          <p>Rs 420</p>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="best">
+      <h1>Indoor</h1>
+      <div class="collection">
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Aloevera</p>
+          <p>Rs 420</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Aloevera</p>
+          <p>Rs 420</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Aloevera</p>
+          <p>Rs 420</p>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </div>
+    
+    <div class="popular" id="gifts">
+      <h1>Gifts</h1>
+      <div class="items">
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Chinese Money Plant</p>
+          <p>Rs 500</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/andrew-reshetov-tXGOhiGBu5I-unsplash.jpg" />
+
+          <p>Cactud</p>
+          <p>Rs 600</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/andrew-reshetov-tXGOhiGBu5I-unsplash.jpg" />
+
+          <p>Calathea</p>
+          <p>Rs 5500</p>
+          <button>Add to cart</button>
+        </div>
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Aloevera</p>
+          <p>Rs 420</p>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    
+    </div>
+
+    <div class="latest">
+      <h1>Latest Blog</h1>
+      <div class="blog" id="blog">
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+          <p>Decorative touches for your design</p>
+          <p>
+            Sem magna ut pharetra vitae duis eu senectus sem risus. Morbi non,
+            semper vestibulum euismod accumsan...
+          </p>
+          <button>Read More</button>
+        </div>
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Decorative touches for your design</p>
+          <p>
+            Sem magna ut pharetra vitae duis eu senectus sem risus. Morbi non,
+            semper vestibulum euismod accumsan...
+          </p>
+          <button>Read More</button>
+        </div>
+        <div class="card">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+
+          <p>Decorative touches for your design</p>
+          <p>
+            Sem magna ut pharetra vitae duis eu senectus sem risus. Morbi non,
+            semper vestibulum euismod accumsan...
+          </p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
+    <div class="customer">
+      <div class="review">
+        <div class="plant">
+          <img src="./image/stephanie-harvey-T0inbt7nRME-unsplash.jpg" />
+        </div>
+        <div class="comment">
+          <h1>
+            Review<i class="fa-solid fa-star"></i
+            ><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas,
+            exercitationem alias totam, dolor ipsa consectetur repellendus
+            cumque, eum et enim velit architecto sunt assumenda veniam omnis
+            rerum labore perferendis nesciunt.
+          </p>
+          <span>Rikesh Shrestha</span>
+        </div>
+      </div>
+    </div>
+    <div class="ship" id="about">
+      <div class="ment">
+        <div>
+          <h1><i class="fa-solid fa-truck-fast"></i> Free Shipping</h1>
+          <p>Capped at $39</p>
+        </div>
+        <div>
+          <h1><i class="fa-solid fa-credit-card"></i> Safe Payment</h1>
+          <p>With our payment gateway</p>
+        </div>
+        <div>
+          <h1><i class="fa-solid fa-credit-card"></i> Quality Guarantee</h1>
+          <p>Fresh and super item available</p>
+        </div>
+        <div>
+          <h1><i class="fa-solid fa-credit-card"></i>Big Offer</h1>
+          <p>We give big offer surely</p>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="foot">
+        <h1>The Growers</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. A architecto
+          dolores fugit tenetur quam vitae ad asperiores est, repellendus
+          maiores accusamus temporibus debitis sunt laudantium alias
+          exercitationem dolore reiciendis fuga.
+        </p>
+        <input type="text" />
+        <button>Submit</button>
+      </div>
+      <div>
+        <h1>Company</h1>
+        <ul>
+          <li>About</li>
+          <li>Our platforms</li>
+          <li>Our vision</li>
+          <li>Installation</li>
+          <li>Refer a friend</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Support</h1>
+
+        <ul>
+          <li>About</li>
+          <li>Our platforms</li>
+          <li>Our vision</li>
+          <li>Installation</li>
+          <li>Refer a friend</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Contact Us</h1>
+        <ul>
+          <li>Sorakhutte,Kathmandu</li>
+          <li>+977-9808452856</li>
+          <li>rikeshmania123@gmail.com</li>
+        </ul>
+      </div>
+    </footer>
+    <script src="index.js"></script>
+  </body>
+</html>
